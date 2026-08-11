84ce594ba65d88d61d3bfacb757ff89c43f3f24b
mdismail8562 <ismailhossan8562@gmail.com>
2026-08-11 09:39:14 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/freetv/",
   plugins: [react()],
-  server: {
-    host: true,
-    port: 5173
-  }
 });
